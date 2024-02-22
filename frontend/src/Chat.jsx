e797787a66bca22646cd90d5715ba3393a17d334
lrtinkiv <lrtinkiv@jyu.fi>
2024-02-22 15:34:05 +0200

    styles to messages

--- frontend/src/Chat.jsx
+++ frontend/src/Chat.jsx
@@ -16,11 +16,26 @@ const Chat = ({ selectedGroup, messages, setMessages }) => {
                 })
         }
     }, [selectedGroup, setMessages])
+
+    // Function to format timestamp
+    function formatTimestamp(timestamp) {
+        const date = new Date(timestamp);
+        const day = ('0' + date.getDate()).slice(-2);
+        const month = ('0' + (date.getMonth() + 1)).slice(-2);
+        const year = date.getFullYear();
+        const hours = ('0' + date.getHours()).slice(-2);
+        const minutes = ('0' + date.getMinutes()).slice(-2);
+        return `${day}.${month}.${year} ${hours}:${minutes}`;
+    }
     
     return (
         <div className="chat">
             {messages.map((message, i)=>(
-                <li key={i}>{message.sender}: {message.text}</li>
+                <li key={i}>
+                    
+                    {message.sender}: {message.text}
+                <p className='timestamp'>{formatTimestamp(message.timestamp)}</p>
+                </li>
             ))}
         </div>
     );
